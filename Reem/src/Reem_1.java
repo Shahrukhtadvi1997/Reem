0a18ebaa65d25b82a95ccbf7608ea132c345e865
Shahrukhtadvi1997 <srktadvi427@gmail.com>
2022-11-15 16:17:38 +0530

    my first

---
+++ Reem/src/Reem_1.java
@@ -0,0 +1,16 @@
+import java.util.HashSet;
+
+public class Reem_1 {
+
+	public static void main(String[] args) {
+	HashSet hs = new HashSet ();
+	hs.add(45);
+	hs.add(78);
+	hs.add(65);
+	hs.add(32);
+	System.out.println(hs);
+	
+
+	}
+
+}
